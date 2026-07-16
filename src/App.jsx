cfb39633e63db22f574bad30724d8fceeddfafcd
Christian <clementchristianunoh@gmail.com>
2026-07-16 08:33:42 +0100

    Add redirect from root to dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import Dashboardlayout from "./Layout/Dashboardlayout";
 import "./App.css";
 import Overview from "./Pages/Overview";
 import Appointment from "./Pages/Appointment";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import React from "react";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/dashboard" replace />} />
       <Route path="/dashboard" element={<Dashboardlayout />}>
         <Route index element={<Overview />} />
         <Route path="appointment" element={<Appointment />} />
